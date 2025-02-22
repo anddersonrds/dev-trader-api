e983e1b82d13f9a88a15bd526c0b8b551e8956b6
Anderson Rodrigues <anddersonrds@gmail.com>
2025-02-22 14:09:21 -0300

    feat: inset assets in wallet

--- src/wallets/entities/wallet.entity.ts
+++ src/wallets/entities/wallet.entity.ts
@@ -1,12 +1,21 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
 import crypto from 'crypto';
+
+import { WalletAsset, WalletAssetDocument } from './wallet-asset.entity';
 
 export type WalletDocument = HydratedDocument<Wallet>;
 @Schema({ timestamps: true })
 export class Wallet {
   @Prop({ default: () => crypto.randomUUID() })
   _id: string;
+
+  @Prop({
+    type: [mongoose.Schema.Types.String],
+    set: (v) => [...new Set(v)],
+    ref: WalletAsset.name,
+  })
+  assets: WalletAssetDocument[] | string[];
 
   createdAt!: Date;
 
--- src/wallets/wallets.service.ts
+++ src/wallets/wallets.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
+import mongoose, { Model } from 'mongoose';
 
 import { Wallet } from './entities/wallet.entity';
 import { WalletAsset } from './entities/wallet-asset.entity';
@@ -13,6 +13,7 @@ export class WalletsService {
     @InjectModel(Wallet.name) private walletSchemma: Model<Wallet>,
     @InjectModel(WalletAsset.name)
     private walletAssetSchemma: Model<WalletAsset>,
+    @InjectConnection() private connection: mongoose.Connection,
   ) {}
 
   create(createWalletDto: CreateWalletDto) {
@@ -24,7 +25,12 @@ export class WalletsService {
   }
 
   findOne(id: string) {
-    return this.walletSchemma.findById(id);
+    return this.walletSchemma.findById(id).populate([
+      {
+        path: 'assets', //walletasset
+        populate: ['asset'],
+      },
+    ]);
   }
 
   /* TODO: This method will be implemented soon
@@ -36,7 +42,41 @@ export class WalletsService {
     return `This action removes a #${id} wallet`;
   }*/
 
-  createWalletAsset(createWalletAssetDto: CreateWalletAssetDto) {
-    return this.walletAssetSchemma.create(createWalletAssetDto);
+  async createWalletAsset(createWalletAssetDto: CreateWalletAssetDto) {
+    const session = await this.connection.startSession();
+
+    await session.startTransaction();
+
+    try {
+      const docs = await this.walletAssetSchemma.create(
+        [
+          {
+            wallet: createWalletAssetDto.walletId,
+            asset: createWalletAssetDto.assetId,
+            shares: createWalletAssetDto.shares,
+          },
+        ],
+        { session },
+      );
+
+      const walletAsset = docs[0];
+
+      await this.walletSchemma.updateOne(
+        { _id: createWalletAssetDto.walletId },
+        {
+          $push: { assets: walletAsset._id },
+        },
+        { session },
+      );
+
+      await session.commitTransaction();
+      return walletAsset;
+    } catch (e) {
+      console.error(e);
+      await session.abortTransaction();
+      throw e;
+    } finally {
+      await session.endSession();
+    }
   }
 }
